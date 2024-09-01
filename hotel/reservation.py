90d3feefd5d2f526d0f84dc588f6cd2141eef781
sayka88 <sayka.alekperova@gmail.com>
2024-09-01 16:53:03 +0000

    reservation.py

--- hotel/reservation.py
+++ hotel/reservation.py
@@ -1,22 +1,34 @@
 class Reservation:
-    def __init__(self, guest, room, check_in, check_out):
+    reservation_counter = 1
+
+    def __init__(self, guest, room, check_in_date, check_out_date):
+        self.reservation_id = Reservation.reservation_counter
+        Reservation.reservation_counter += 1
         self.guest = guest
         self.room = room
-        self.check_in = check_in
-        self.check_out = check_out
+        self.check_in_date = check_in_date
+        self.check_out_date = check_out_date
         self.total_cost = self.calculate_total_cost()
 
     def calculate_total_cost(self):
-        duration = self.check_out - self.check_in
-        return duration * self.room.rate
+        num_days = (self.check_out_date - self.check_in_date).days
+        return num_days * self.room.rate_per_night
 
     def modify_reservation(self, new_check_in, new_check_out):
-        self.room.update_availability(self.check_in, self.check_out, True)
-        self.check_in = new_check_in
-        self.check_out = new_check_out
-        self.room.update_availability(self.check_in, self.check_out, False)
+        self.check_in_date = new_check_in
+        self.check_out_date = new_check_out
         self.total_cost = self.calculate_total_cost()
 
     def cancel_reservation(self):
-        self.room.update_availability(self.check_in, self.check_out, True)
+        self.room.update_availability(True)
+
+    def get_reservation_details(self):
+        return {
+            'reservation_id': self.reservation_id,
+            'guest': self.guest.name,
+            'room_number': self.room.room_number,
+            'check_in_date': self.check_in_date,
+            'check_out_date': self.check_out_date,
+            'total_cost': self.total_cost
+        }
 
