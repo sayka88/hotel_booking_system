ebf4f20aac6afe5c9655ab4d978d2bd6b865e73f
sayka88 <sayka.alekperova@gmail.com>
2024-09-01 17:26:17 +0000

    room.py

--- hotel/room.py
+++ hotel/room.py
@@ -1,8 +1,8 @@
 class Room:
-    def __init__(self, room_number, room_type, rate, availability=True):
+    def __init__(self, room_number, room_type, rate_per_night, availability=True):
         self.room_number = room_number
         self.room_type = room_type
-        self.rate = rate
+        self.rate_per_night = rate_per_night  # Переименовано в rate_per_night
         self.availability = availability
 
     def check_availability(self):
@@ -12,18 +12,18 @@ class Room:
         self.availability = status
 
     def get_room_details(self):
-        return f"Room {self.room_number}: {self.room_type}, Rate: {self.rate}, Available: {self.availability}"
+        return f"Room {self.room_number}: {self.room_type}, Rate: {self.rate_per_night}, Available: {self.availability}"
 
 # Определение подклассов для разных типов комнат
 class SingleRoom(Room):
-    def __init__(self, room_number, rate, availability=True):
-        super().__init__(room_number, "Single", rate, availability)
+    def __init__(self, room_number, rate_per_night, availability=True):
+        super().__init__(room_number, "Single", rate_per_night, availability)
 
 class DoubleRoom(Room):
-    def __init__(self, room_number, rate, availability=True):
-        super().__init__(room_number, "Double", rate, availability)
+    def __init__(self, room_number, rate_per_night, availability=True):
+        super().__init__(room_number, "Double", rate_per_night, availability)
 
 class Suite(Room):
-    def __init__(self, room_number, rate, availability=True):
-        super().__init__(room_number, "Suite", rate, availability)
+    def __init__(self, room_number, rate_per_night, availability=True):
+        super().__init__(room_number, "Suite", rate_per_night, availability)
 
