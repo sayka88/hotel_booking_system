d1fdfef2577eb426d07923d6e08ece4a15b2d1fb
sayka88 <sayka.alekperova@gmail.com>
2024-09-01 16:53:56 +0000

    hotel.py

--- hotel/hotel.py
+++ hotel/hotel.py
@@ -1,5 +1,3 @@
-from .reservation import Reservation
-
 class Hotel:
     def __init__(self, name, location):
         self.name = name
@@ -10,19 +8,28 @@ class Hotel:
     def add_room(self, room):
         self.rooms.append(room)
 
-    def remove_room(self, room):
-        self.rooms.remove(room)
+    def remove_room(self, room_number):
+        self.rooms = [room for room in self.rooms if room.room_number != room_number]
 
-    def get_available_rooms(self, check_in, check_out):
+    def find_available_rooms(self, room_type, check_in_date, check_out_date):
         available_rooms = []
         for room in self.rooms:
-            if room.is_available(check_in, check_out):
+            if room.room_type == room_type and room.check_availability():
                 available_rooms.append(room)
         return available_rooms
 
-    def make_reservation(self, guest, room, check_in, check_out):
-        reservation = Reservation(guest, room, check_in, check_out)
-        self.reservations.append(reservation)
-        room.update_availability(check_in, check_out, False)
-        return reservation
+    def make_reservation(self, guest, room_number, check_in_date, check_out_date):
+        room = next((room for room in self.rooms if room.room_number == room_number and room.check_availability()), None)
+        if room:
+            reservation = Reservation(guest, room, check_in_date, check_out_date)
+            room.update_availability(False)
+            self.reservations.append(reservation)
+            guest.add_reservation(reservation)
+            return reservation
+        else:
+            print(f"Room {room_number} is not available.")
+            return None
+
+    def view_all_reservations(self):
+        return [reservation.get_reservation_details() for reservation in self.reservations]
 
