02a36a709ebec85aa65e626662ca07a5eeb9300c
sayka88 <sayka.alekperova@gmail.com>
2024-09-01 17:26:05 +0000

    hotel.py

--- hotel/hotel.py
+++ hotel/hotel.py
@@ -1,3 +1,7 @@
+from .room import Room, SingleRoom, DoubleRoom, Suite
+from .reservation import Reservation
+from .guest import Guest
+
 class Hotel:
     def __init__(self, name, location):
         self.name = name
@@ -8,28 +12,19 @@ class Hotel:
     def add_room(self, room):
         self.rooms.append(room)
 
-    def remove_room(self, room_number):
-        self.rooms = [room for room in self.rooms if room.room_number != room_number]
-
-    def find_available_rooms(self, room_type, check_in_date, check_out_date):
-        available_rooms = []
-        for room in self.rooms:
-            if room.room_type == room_type and room.check_availability():
-                available_rooms.append(room)
-        return available_rooms
-
     def make_reservation(self, guest, room_number, check_in_date, check_out_date):
-        room = next((room for room in self.rooms if room.room_number == room_number and room.check_availability()), None)
-        if room:
+        room = self.find_room(room_number)
+        if room and room.check_availability():
             reservation = Reservation(guest, room, check_in_date, check_out_date)
-            room.update_availability(False)
             self.reservations.append(reservation)
-            guest.add_reservation(reservation)
+            room.update_availability(False)
             return reservation
         else:
-            print(f"Room {room_number} is not available.")
-            return None
+            raise ValueError("Room is not available or does not exist.")
 
-    def view_all_reservations(self):
-        return [reservation.get_reservation_details() for reservation in self.reservations]
+    def find_room(self, room_number):
+        for room in self.rooms:
+            if room.room_number == room_number:
+                return room
+        return None
 
